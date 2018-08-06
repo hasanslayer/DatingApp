21292900b575d88d4885aab26d978d7d1175a3b7
hasanslayer <hasan-dr2010@hotmail.com>
2018-08-06 11:26:10 +0200

    Create Dating Repository

---
+++ DatingApp.API/Data/IDatingRepository.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DatingApp.API.Models;
+
+namespace DatingApp.API.Data
+{
+    public interface IDatingRepository
+    {
+        void Add<T>(T entity) where T : class; // single generic add method (user,photo,...,etc)
+        void Delete<T>(T entity) where T : class;
+        Task<bool> SaveAll();
+        Task<IEnumerable<User>> GetUsers();
+        Task<User> GerUser(int id);
+    }
+}
