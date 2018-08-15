cd86d63fe74b76c71f3941ed60d51985480dcf80
hasanslayer <hasan-dr2010@hotmail.com>
2018-08-15 03:24:48 +0200

    Configuring the 3rd Party file uploader

--- DatingApp.SPA/src/app/members/photo-editor/photo-editor.component.ts
+++ DatingApp.SPA/src/app/members/photo-editor/photo-editor.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Photo } from '../../_models/Photo';
 import { FileUploader } from 'ng2-file-upload';
+import { environment } from '../../../environments/environment';
+import { AuthService } from '../../_services/auth.service';
 
 @Component({
   selector: 'app-photo-editor',
@@ -10,14 +12,47 @@ import { FileUploader } from 'ng2-file-upload';
 export class PhotoEditorComponent implements OnInit {
   @Input()
   photos: Photo[];
-  uploader: FileUploader = new FileUploader({});
+  uploader: FileUploader;
   hasBaseDropZoneOver = false;
+  baseUrl = environment.apiUrl;
 
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.initializeUploader();
+  }
 
   public fileOverBase(e: any) {
     this.hasBaseDropZoneOver = e; // this for drag and drop functionality
+  }
+
+  initializeUploader() {
+    this.uploader = new FileUploader({
+      url:
+        this.baseUrl +
+        'users/' +
+        this.authService.decodedToken.nameid +
+        '/photos',
+      authToken: 'Bearer' + localStorage.getItem('token'),
+      isHTML5: true,
+      allowedFileType: ['image'],
+      removeAfterUpload: true,
+      autoUpload: false, // click the button to upload the image
+      maxFileSize: 10 * 1024 * 1024 // 10 Mb
+    });
+
+    this.uploader.onSuccessItem = (item, response, status, headers) => {
+      if (response) {
+        const res: Photo = JSON.parse(response);
+        const photo = {
+          id: res.id,
+          url: res.url,
+          dateAdded: res.dateAdded,
+          description: res.description,
+          isMain: res.isMain
+        };
+        this.photos.push(photo);
+      }
+    };
   }
 }
