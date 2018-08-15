2d8f73c90486ee49ba10a7b0ce156f8211b65683
hasanslayer <hasan-dr2010@hotmail.com>
2018-08-15 03:55:59 +0200

    Adding the Set Main photo functionality to the API

--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Models;
 using Microsoft.EntityFrameworkCore;
@@ -27,6 +28,11 @@ namespace DatingApp.API.Data
         {
             var user = await _context.Users.Include(p => p.Photos).FirstOrDefaultAsync(u => u.Id == id);
             return user;
+        }
+
+        public Task<Photo> GetMainPhotoForUser(int userId)
+        {
+            return _context.Photos.Where(u => u.UserId == userId).FirstOrDefaultAsync(p => p.IsMain);
         }
 
         public Task<Photo> GetPhoto(int id)
